9fb663c221cee3898216460eab0cba71fffa6290
nika <nika.gogua8@gmail.com>
2023-01-28 21:33:53 +0400

    header styling

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import classes from "./Header.module.css";
+import meals from "../assets/meals.jpg";
+import React from "react";
+import HeaderButton from "./HeaderButoon";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt="meals" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;

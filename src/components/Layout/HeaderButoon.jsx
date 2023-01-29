430834bb5cec0e8d7a6946ddc2da2f7927b73e94
nika <nika.gogua8@gmail.com>
2023-01-29 15:39:37 +0400

    remove items and effect

--- src/components/Layout/HeaderButoon.jsx
+++ src/components/Layout/HeaderButoon.jsx
@@ -1,17 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderButton = (props) => {
   const ctx = useContext(CartContext);
+  const [isBump, setIsBump] = useState(false);
 
   const numberOfCartItems = ctx.items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
 
+  const btnClass = `${classes.button} ${isBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+
+    setIsBump(true);
+
+    const timer = setTimeout(() => setIsBump(false), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -35,6 +35,23 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updateItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updateItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
 
   return defaultCartState;

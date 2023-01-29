b369bd0c554b93953c95e3f219ed62fdbd3ca73a
nika <nika.gogua8@gmail.com>
2023-01-29 13:29:19 +0400

    implement onclick for cart items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsVisible(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsVisible(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      {cartIsVisible && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <Meals />
     </React.Fragment>
   );
--- src/components/Layout/HeaderButoon.jsx
+++ src/components/Layout/HeaderButoon.jsx
@@ -3,7 +3,7 @@ import classes from "./HeaderButton.module.css";
 
 const HeaderButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
